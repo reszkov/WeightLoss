993476fc8f2968092c4800d512a3f7ee667425c8
plareszka <plareszka@HILDZL57103>
2016-09-01 13:28:58 +0200

    Add project files.

---
+++ WeightLoss/Controllers/CuisineController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeightLoss.Controllers
+{                   
+    public class CuisineController : Controller
+    {
+      [Authorize]
+        public ActionResult Search(string name = "Polish")
+        {
+            var message = Server.HtmlEncode(name);
+                return Content(message);
+        }
+
+        //public ActionResult Search()
+        //{
+         
+        //    return Content("XaXa");
+        //}
+
+    }
+}
